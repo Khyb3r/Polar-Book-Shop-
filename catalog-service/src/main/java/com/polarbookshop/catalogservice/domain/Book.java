5ecd1341b45a0940ad6bea65384c56338b65470d
Khyb3r <157541714+Khyb3r@users.noreply.github.com>
2025-08-01 21:59:23 +0100

    Validation for Input in Book Records added

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -1,8 +1,23 @@
 package com.polarbookshop.catalogservice.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+
 public record Book (
+  @NotBlank(message = "Book ISBN must be defined.")
+  @Pattern(
+    regexp = "^([0-9]{10} | [0-9] {13})$",
+    message = "ISBN format must be valid.")
   String isbn,
+
+  @NotBlank(message = "Book Title must be defined.")
   String title,
+
+  @NotBlank(message = "Book Author must be defined.")
   String author,
+
+  @NotBlank(message = "Book Price must be defined.")
+  @Positive(message = "Price must be greater than Zero.")
   Double price
 ) {}
