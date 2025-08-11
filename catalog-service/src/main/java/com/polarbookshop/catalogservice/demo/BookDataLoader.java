ad73809419916271479dcc3c09e182a3b3a51b2b
Khyb3r <157541714+Khyb3r@users.noreply.github.com>
2025-08-11 02:53:34 +0100

    Cloud config client setup done and connection as well as resilience of the connection to the config server done

---
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -0,0 +1,26 @@
+package com.polarbookshop.catalogservice.demo;
+
+import com.polarbookshop.catalogservice.domain.Book;
+import com.polarbookshop.catalogservice.domain.BookRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("testdata")
+public class BookDataLoader {
+    private final BookRepository bookRepository;
+
+    public BookDataLoader(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadBookTestData() {
+        var book1 = new Book("1234567890", "Title", "Mr.A", 10.99);
+        var book2 = new Book("0987654321", "Title2", "Mr.B", 100.99);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+    }
+}
